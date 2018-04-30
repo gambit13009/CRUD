31264da87e4ec039d4d562e1cb26513dbac3ac5b
Jerome David <jerome.david@cegetel.net>
2018-04-30 16:18:20 +0200

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-movies = Movie.create(title: "StarWars", release_year: "1977", director: "Geroges Lucas")
-movies = Movie.create(title: "Matrix", release_year: "1999", director: "Wachowski brothers")
-movies = Movie.create(title: "Lordoftherings", release_year: "2001", director: "Peter Jackson")
-movies = Movie.create(title: "PulpFiction", release_year: "1994", director: "Quentin Tarantino")
-movies = Movie.create(title: "JurassicPark", release_year: "1993", director: "Steven Spielberg")
+
+movies = Movie.create(title: "StarWars", release_year: 1977, director: "Geroges Lucas")
+movies = Movie.create(title: "Matrix", release_year: 1999, director: "Wachowski brothers")
+movies = Movie.create(title: "Lordoftherings", release_year: 2001, director: "Peter Jackson")
+movies = Movie.create(title: "PulpFiction", release_year: 1994, director: "Quentin Tarantino")
+movies = Movie.create(title: "JurassicPark", release_year: 1993, director: "Steven Spielberg")
+
